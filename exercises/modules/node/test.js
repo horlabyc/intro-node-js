5d7ce9904ec62f893fec849a739ae9376d78da59
Sulaiman <sulaimain@kobo360.com>
2020-10-25 18:23:51 +0100

    Solution to exercise

--- exercises/modules/node/test.js
+++ exercises/modules/node/test.js
@@ -1,6 +1,6 @@
 const data = require('./data')
-const api = require('./api')
-const app = require('./app')
+const {getUserById, getPostsForUser} = require('./api')
+const { showPostsForCurrentUser, showUserProfile} = require('./app')
 
 describe('data', () => {
   test('users',  () => {
@@ -14,13 +14,13 @@ describe('data', () => {
 describe('api', () => {
   test('getUserById', done => {
     expect.assertions(1)
-    api.getUserById(1, user => {
+    getUserById(1, user => {
       expect(user.id).toBe(1)
       done()
     })
   })
   test('getPostsForUser', done => {
-    api.getPostsForUser(1, posts => {
+    getPostsForUser(1, posts => {
       expect(posts).toHaveLength(3)
       posts.forEach(post => {
         expect(post.createdBy).toBe(1)
@@ -32,14 +32,14 @@ describe('api', () => {
 
 describe('app', () => {
   test('showPostsForCurrentUser', done => {
-    app.showPostsForCurrentUser(1, posts => {
+    showPostsForCurrentUser(1, posts => {
       expect(posts).toHaveLength(3)
       done()
     })
   })
 
   test('showUserProfile', done => {
-    app.showUserProfile(1, profile => {
+    showUserProfile(1, profile => {
       expect(profile).toBeTruthy()
       done()
     })
